89dab0f94067868fcfa4eb65583d14967073b38f
Dmitri Morozov <dmitri.m80@gmail.com>
2023-05-26 15:06:34 -0400

    Initial Commit

---
+++ PaddingAndSpacers/PaddingAndSpacersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaddingAndSpacersApp.swift
+//  PaddingAndSpacers
+//
+//  Created by Dmitri Morozov on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct PaddingAndSpacersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
